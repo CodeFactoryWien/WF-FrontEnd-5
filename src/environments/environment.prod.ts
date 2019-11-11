6f98cbf77cff62b53be5fd55ddd33d77ed7da4da
drewitzmax <drewitz.max@googlemail.com>
2019-11-11 13:13:02 +0100

    add bootstrap to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyAdlTz5zoLH7zuDLeWg1Hiq4wbQAZ8vt0A",
+    authDomain: "thephonebookcompany.firebaseapp.com",
+    databaseURL: "https://thephonebookcompany.firebaseio.com",
+    projectId: "thephonebookcompany",
+    storageBucket: "thephonebookcompany.appspot.com",
+    messagingSenderId: "682588378279",
+    appId: "1:682588378279:web:f6b63b669b154ca5f27782",
+    measurementId: "G-XVS8CT8BG4"
+  }
 };
