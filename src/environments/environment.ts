fa3d9cd154d798b7da38555099519eccf4c795ef
drewitzmax <drewitz.max@googlemail.com>
2019-11-11 10:31:10 +0100

    added firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyAdlTz5zoLH7zuDLeWg1Hiq4wbQAZ8vt0A",
+    authDomain: "thephonebookcompany.firebaseapp.com",
+    databaseURL: "https://thephonebookcompany.firebaseio.com",
+    projectId: "thephonebookcompany",
+    storageBucket: "thephonebookcompany.appspot.com",
+    messagingSenderId: "682588378279",
+    appId: "1:682588378279:web:f6b63b669b154ca5f27782",
+    measurementId: "G-XVS8CT8BG4"
+  }
 };
 
 /*
