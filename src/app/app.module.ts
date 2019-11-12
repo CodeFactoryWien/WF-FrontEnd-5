646d23e4c2b5824bdcee5a04a5de165d10eb4843
Steven Markhardt <steven.markhardt@gmx.at>
2019-11-12 13:32:24 +0100

    fixed merge :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,6 +12,11 @@ import { FooterComponent } from './footer/footer.component';
 import { PhoneBookComponent } from './phone-book/phone-book.component';
 import { PhoneListComponent } from './phone-list/phone-list.component';
 import { PhonebookService} from "./shared/phonebook.service";
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
@@ -21,7 +28,10 @@ import { environment } from "../environments/environment";
     NavbarComponent,
     FooterComponent,
     PhoneBookComponent,
-    PhoneListComponent
+    PhoneListComponent,
+    HomeComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -29,9 +39,17 @@ import { environment } from "../environments/environment";
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule ,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
+
   ],
-  providers: [PhonebookService],
+  providers: [
+    PhonebookService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiUrl: 'http://localhost:4000',
   firebaseConfig :{
     apiKey: "AIzaSyAdlTz5zoLH7zuDLeWg1Hiq4wbQAZ8vt0A",
     authDomain: "thephonebookcompany.firebaseapp.com",
